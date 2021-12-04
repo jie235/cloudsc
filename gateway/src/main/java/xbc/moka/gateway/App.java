4fd2181d97a9c883cd6acff1a91103ee7ab9ddf4
csc <csc@github.com>
2021-12-04 11:19:25 +0800

    feat: gateway 自带限流方案

--- gateway/src/main/java/xbc/moka/gateway/App.java
+++ gateway/src/main/java/xbc/moka/gateway/App.java
@@ -3,11 +3,19 @@ package xbc.moka.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+    }
+
+    @Bean
+    KeyResolver ipKeyResolver(){
+        return exchange -> Mono.just(exchange.getRequest().getRemoteAddress().getHostName());
     }
 }
